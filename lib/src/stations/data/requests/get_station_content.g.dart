e5c82a2b72db3e812d1c12e08b143e57f4688d4e
hacker1024 <hacker1024@users.sourceforge.net>
2022-04-28 22:52:27 +1000

    chore: Disable all lints in combined generated files

--- lib/src/stations/data/requests/get_station_content.g.dart
+++ lib/src/stations/data/requests/get_station_content.g.dart
@@ -1,5 +1,7 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
+// ignore_for_file: type=lint
+
 part of 'get_station_content.dart';
 
 // **************************************************************************
