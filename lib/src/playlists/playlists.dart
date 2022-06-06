5de1252a352be2c6300d6c0be09ac044d9abbc97
hacker1024 <hacker1024@users.sourceforge.net>
2022-06-06 17:13:26 +1000

    feat: Expose playlist entities

--- lib/src/playlists/playlists.dart
+++ lib/src/playlists/playlists.dart
@@ -1,1 +1,5 @@
 export 'api/playlists.dart';
+export 'entities/listener_id_info.dart';
+export 'entities/playlist_segment.dart';
+export 'entities/playlist_track.dart';
+export 'entities/playlist_viewer_info.dart';
