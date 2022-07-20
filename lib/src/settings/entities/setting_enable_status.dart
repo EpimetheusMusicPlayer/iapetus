130ceab52183ddc723ec3bb43ecf539439f8f265
hacker1024 <hacker1024@users.sourceforge.net>
2022-07-20 11:53:21 +1000

    feat: Add value property to SettingEnableStatus

--- lib/src/settings/entities/setting_enable_status.dart
+++ lib/src/settings/entities/setting_enable_status.dart
@@ -2,11 +2,16 @@ import 'package:json_annotation/json_annotation.dart';
 
 enum SettingEnableStatus {
   @JsonValue('true')
-  yes,
+  yes(true),
   @JsonValue('false')
-  no,
+  no(false),
   @JsonValue('')
-  enabled,
+  enabled(true),
   @JsonValue('disabled')
-  disabled,
+  disabled(false);
+
+  final bool value;
+
+  // ignore: avoid_positional_boolean_parameters
+  const SettingEnableStatus(this.value);
 }
